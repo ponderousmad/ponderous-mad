7fca53ea13bdbee351cf12bb5391367703feb511
Adrian Smith <agnomen@gmail.com>
2015-12-05 12:24:58 -0500

    Adjust path to point to appropriate location for app engine.

--- goprompt.bat
+++ goprompt.bat
@@ -1,4 +1,4 @@
-set path=%path%;D:\Make12\go_appengine;c:\python27
+set path=%path%;D:\progfiles\go_appengine;c:\python27
 D:
 cd \github\ponderous-mad
 start echo Starting ponderous-mad development command line....
