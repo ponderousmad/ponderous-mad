de10d99b1f7d33864582647ffd4270d51ba0c450
Adrian Smith <agnomen@gmail.com>
2015-12-31 13:17:38 -0500

    Start prompt in appengine

--- goprompt.bat
+++ goprompt.bat
@@ -1,4 +1,4 @@
 set path=%path%;D:\progfiles\go_appengine;c:\python27
 D:
-cd \github\ponderous-mad
+cd \github\ponderous-mad\appengine
 start echo Starting ponderous-mad development command line....
